62ed5c492dafec874ed6a17f1f627c301aa158c1
Alexandra_Rybakova <kuznetzvi@mail.ru>
2022-08-14 21:45:56 +0300

    final_result

--- parcing_part_2.py
+++ parcing_part_2.py
@@ -1,3 +1,8 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import json
+
+%%time
 URL_TEMPLATE = "https://www.imdb.com/search/name/?gender=male%2Cfemale&ref_=nv_cel_m"
 URL_start = 'https://www.imdb.com'
 r = requests.get(URL_TEMPLATE)
@@ -11,14 +16,10 @@ for name in actors_names:
     soup_film = bs(r_film.text, "html.parser")
     res_film = soup_film.find('div', id ="filmography").find_all('a', href = True, class_ = None, limit = 15)
     
-    result_list.append({'movies': [res_film[i].get_text() for i in range(len(res_film))], \
-                        'movie_links': [(URL_start + res_film[i].get('href')) for i in range(len(res_film))]})
-    
     for i in range(len(res_film)):
         r_actors = requests.get(URL_start + res_film[i].get('href'))
         soup = bs(r_actors.text, "html.parser")
         res = soup.find_all('div',attrs={"data-testid":"title-cast-item"})
-        result_list.append({'movies': res_film[i].get_text(), \
-                            'movie_links': (URL_start + res_film[i].get('href'))})
-        for i in res:
-            result_list.append({'cast': i.find('a').get('aria-label')})
+        result_list.append({'movie_links': (URL_start + res_film[i].get('href')),
+                            'title': res_film[i].get_text().strip(),  
+                            'cast': [i.find('a').get('aria-label').strip() for i in res]})
